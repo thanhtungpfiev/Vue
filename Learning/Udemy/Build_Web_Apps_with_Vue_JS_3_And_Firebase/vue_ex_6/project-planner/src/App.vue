2acaf57897866ff0c206cb1df5f98f9156141255
Dao Thanh Tung (MS/EDA43-XC) <tung.daothanh@vn.bosch.com>
2024-05-31 09:01:52 +0700

    Udemy Build Web Apps with Vue JS 3 & Firebase Vue PROJECT BUILD Simple Project Planner

---
+++ Learning/Udemy/Build_Web_Apps_with_Vue_JS_3_And_Firebase/vue_ex_6/project-planner/src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <NavBar></NavBar>
+  <router-view />
+</template>
+
+<script>
+import NavBar from './components/NavBar.vue';
+
+export default {
+  components: {
+    NavBar
+  }
+}
+</script>
+
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  max-width: 600px;
+  margin: 0 auto;
+  color: #555;
+}
+</style>
