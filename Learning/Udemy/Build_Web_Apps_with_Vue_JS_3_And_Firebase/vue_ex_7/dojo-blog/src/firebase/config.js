242bf6eba8a083661295f7647fd331f9f32fd89f
thanhtungpfiev <thanhtungpfiev@gmail.com>
2024-06-06 11:32:06 +0700

    chore: Update Firebase configuration with new credentials

--- Learning/Udemy/Build_Web_Apps_with_Vue_JS_3_And_Firebase/vue_ex_7/dojo-blog/src/firebase/config.js
+++ Learning/Udemy/Build_Web_Apps_with_Vue_JS_3_And_Firebase/vue_ex_7/dojo-blog/src/firebase/config.js
@@ -2,17 +2,17 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBXtNf-rupi7DheKW_s_gqdwAKTQ1ygPLE",
-  authDomain: "khq-vue-tutorial.firebaseapp.com",
-  projectId: "khq-vue-tutorial",
-  storageBucket: "khq-vue-tutorial.appspot.com",
-  messagingSenderId: "793965647816",
-  appId: "1:793965647816:web:2c9edded419cdb44de12fd"
-}
+  apiKey: "AIzaSyDFuB1BLl6WvIGVzpfStW63V3EXnKWR7TE",
+  authDomain: "udemy-vue-firebase-sites-c4412.firebaseapp.com",
+  projectId: "udemy-vue-firebase-sites-c4412",
+  storageBucket: "udemy-vue-firebase-sites-c4412.appspot.com",
+  messagingSenderId: "603564741386",
+  appId: "1:603564741386:web:32f810d785bba5dfb9e0f3"
+};
 
 // init firebase
-initializeApp(firebaseConfig)
+const firebaseApp = initializeApp(firebaseConfig)
 
 // init firestore service
-const projectFirestore = getFirestore()
+const projectFirestore = getFirestore(firebaseApp)
 export { projectFirestore }
